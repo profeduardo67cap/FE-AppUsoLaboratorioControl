30691368853bd81d9a5c185299ba6ec7bcb89c79
profeduardo67 <linuxsoft.cap@gmail.com>
2022-12-22 18:40:19 -0600

    Se empieza a configurar La barra de navegación

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
 
+export class NavBarComponent implements OnInit{
+  img='https://commons.wikimedia.org/wiki/File:Angular_full_color_logo.svg';
+
+  constructor(){
+  }
+
+  ngOnInit(): void{
+
+  }
 }
