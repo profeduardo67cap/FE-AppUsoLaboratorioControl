bceb3b73b42e4418771ae58c59079ccef5a2cb69
profeduardo67 <linuxsoft.cap@gmail.com>
2022-12-26 13:59:47 -0600

    Se crea sistema básico de Rutas
    Se crea sistema probable de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CapturarCurpComponent } from './components/capturar-curp/capturar-curp.component';
+import { MostrarReporteComponent } from './components/mostrar-reporte/mostrar-reporte.component';
+import { MostrarTiempoDeUsoComponent } from './components/mostrar-tiempo-de-uso/mostrar-tiempo-de-uso.component';
+import { MuestraPersonaEncontradaComponent } from './components/muestra-persona-encontrada/muestra-persona-encontrada.component';
+import { RegistrarUsoComponent } from './components/registrar-uso/registrar-uso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CapturarCurpComponent},
+  { path: 'registrar', component: RegistrarUsoComponent },
+  { path: 'mostrartiempodeuso/:id', component: MostrarTiempoDeUsoComponent },
+  { path: 'mostrarpersonaencontrada', component: MuestraPersonaEncontradaComponent },
+  { path: 'mostrarreporte', component: MostrarReporteComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }//El path match lo utilizaremos cuano tengamos niveles de rutas
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
