0d9b5826997a23618ce887be09d026e1f52a030e
profeduardo67 <linuxsoft.cap@gmail.com>
2022-12-26 13:52:42 -0600

    Creamos una lista HardCode
    La lista está basada en la interfaz, para consumirla desde aca

--- src/app/components/mostrar-reporte/mostrar-reporte.component.ts
+++ src/app/components/mostrar-reporte/mostrar-reporte.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usos } from 'src/app/interfaces/Usos';
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'app-mostrar-reporte',
   templateUrl: './mostrar-reporte.component.html',
   styleUrls: ['./mostrar-reporte.component.css']
 })
-export class MostrarReporteComponent {
+export class MostrarReporteComponent implements OnInit {
+
+  listaUsos: Usos[] = [
+    { id: '0001', FechaDeUso: new Date(), HoraDeEntrada: '9:20 pm', HoraDeSalida: '10:30 pm'},
+    { id: '0002', FechaDeUso: new Date(), HoraDeEntrada: '11:20 pm', HoraDeSalida: '12:30 pm'},
+    { id: '0003', FechaDeUso: new Date(), HoraDeEntrada: '15:20 pm', HoraDeSalida: '17:30 pm'},
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
